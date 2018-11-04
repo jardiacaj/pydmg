d8b217736ad58a0c98a8a2c2b1eec40a9501ecca
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2018-11-04 19:36:03 +0100

    Entry refactor and pypy

--- pydmg.py
+++ pydmg.py
@@ -1,3 +1,6 @@
+import argparse
+import logging
+
 from cpu import CPU
 from ram import RAM
 
@@ -12,3 +15,30 @@ class PyDMG:
     def run(self):
         while True:
             self.cpu.tick()
+
+
+if __name__ == "__main__":
+    print("PyDMG starting")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("romfile")
+    parser.add_argument("--boot-romfile", default="dmg_boot.bin")
+    parser.add_argument("--log-level",
+                        default=logging._levelToName[logging.DEBUG],
+                        choices=(
+                            logging._levelToName[logging.DEBUG],
+                            logging._levelToName[logging.INFO],
+                            logging._levelToName[logging.WARNING],
+                            logging._levelToName[logging.ERROR],
+                            logging._levelToName[logging.CRITICAL],
+                        ))
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging._nameToLevel[args.log_level])
+
+    emulator = PyDMG(
+        bootromfile_path=args.boot_romfile,
+        romfile_path=args.romfile
+    )
+
+    emulator.run()
