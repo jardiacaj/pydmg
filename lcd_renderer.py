daf3af961b8e9b4179250fc6f0ad074fc3dc09a4
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2018-11-17 18:41:28 +0100

    pygame tile viewer

--- lcd_renderer.py
+++ lcd_renderer.py
@@ -4,11 +4,11 @@ import math
 import numpy as np
 import pygame
 
-color_code_to_gl_color = [
-    (0, 0, 0),
-    (0x55, 0x55, 0x55),
-    (0xAA, 0xAA, 0xAA),
-    (0xFF, 0xFF, 0xFF),
+color_code_to_screen_color = [
+    0,
+    0x555555,
+    0xAAAAAA,
+    0xFFFFFF,
 ]
 
 color_map = []
@@ -24,22 +24,30 @@ for input_byte_1 in range(256):
             code_color = (bit_from_byte_1 * 2) + bit_from_byte_2
             byte_1_copy >>= 1
             byte_2_copy >>= 1
-            tmp_color_list.append(color_code_to_gl_color[code_color])
-        color_map[input_byte_1].append([item for sublist in tmp_color_list for item in sublist])
+            tmp_color_list.append(color_code_to_screen_color[code_color])
+        color_map[input_byte_1].append(tmp_color_list)
 
 
 def get_color_code(byte1, byte2, offset):
     return (((byte2 >> offset) & 1) << 1) + ((byte1 >> offset) & 1)
 
 
+def draw_vertical_line(screen, x, length, color):
+    for i in range(length):
+        screen[x][0:] = [color] * length
+
+
+def draw_horizontal_line(screen, y, length, color):
+    for i in range(length):
+        screen[i][y] = color
+
 class LCDRenderer:
-    def __init__(self, lcd, zoom=4):
+    def __init__(self, lcd):
         self.lcd = lcd
-        self.zoom = zoom
         self.tiles_per_row_in_tile_window = 16
         self.tile_window_size = (
-            (self.tiles_per_row_in_tile_window * 8 + 1) * self.zoom,
-            math.ceil(192 / self.tiles_per_row_in_tile_window * 8) * self.zoom
+            (self.tiles_per_row_in_tile_window * 9),
+            math.ceil(192 / self.tiles_per_row_in_tile_window * 9)
         )
 
         pygame.init()
@@ -48,19 +56,28 @@ class LCDRenderer:
 
     def render(self):
         logging.debug("Rendering frame")
+        self.render_tile_view()
 
-        screenarray = np.zeros(self.tile_window_size)
-        screenarray = np.random.randint(self.tile_window_size[0]*self.tile_window_size[1], size=self.tile_window_size).astype('uint32')
+    def render_tile_view(self):
+        screenarray = np.full(shape=self.tile_window_size,
+                              fill_value=2 ** 32 - 1)
+        for i in range(self.tiles_per_row_in_tile_window):
+            draw_vertical_line(screenarray, i*9, self.tile_window_size[1], 0)
+        for i in range(math.ceil(192 / self.tiles_per_row_in_tile_window)):
+            draw_horizontal_line(screenarray, i*9, self.tile_window_size[0], 0)
+
+        for tile_idx in range(192):
+            tile_base_address = tile_idx * 16 + 0x8000
+            x_offset = (tile_idx % self.tiles_per_row_in_tile_window) * 9
+            for tile_line_number in range(8):
+                y_offset = ((tile_idx // self.tiles_per_row_in_tile_window) * 9 + tile_line_number)
+                line_base_address = tile_base_address + tile_line_number * 2
+                color_byte_1 = self.lcd.memory.read(line_base_address)
+                color_byte_2 = self.lcd.memory.read(line_base_address + 1)
+                line_output_colors = color_map[color_byte_1][color_byte_2]
+                for i in range(len(line_output_colors)):
+                    screenarray[x_offset+i][y_offset] = line_output_colors[i]
+
+
         pygame.surfarray.blit_array(self.screen, screenarray)
         pygame.display.flip()
-
-    def draw_tile(self, tile_idx):
-        tile_base_address = tile_idx * 16 + 0x8000
-        x_offset = (tile_idx % 16) * 8 * self.zoom
-        for tile_line_number in range(8):
-            line_base_address = tile_base_address + tile_line_number * 2
-            color_byte_1 = self.memory.read(line_base_address)
-            color_byte_2 = self.memory.read(line_base_address + 1)
-            line_output_colors = color_map[color_byte_1][color_byte_2]
-
-            y_offset = ((tile_idx // 16 + 1) * 8 - tile_line_number) * self.zoom
